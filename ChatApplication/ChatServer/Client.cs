536e07d22af5cd423de6792d60a9bc4afc4585ed
Moustapha Azaïmi <m.azaimi@student.avans.nl>
2023-01-27 12:25:21 +0100

    add client class for temp server

---
+++ ChatApplication/ChatServer/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+
+namespace ChatServer;
+
+public class Client
+{
+    public string Username { get; set; }
+    public Guid UID { get; set; }
+    public TcpClient ClientSocket { get; set; }
+
+    public Client(TcpClient client)
+    {
+        ClientSocket = client;
+        UID = Guid.NewGuid();
+        
+        
+        Console.WriteLine($"{DateTime.Now} - Client connected with username {Username} and UID {UID}");
+    }
+}
