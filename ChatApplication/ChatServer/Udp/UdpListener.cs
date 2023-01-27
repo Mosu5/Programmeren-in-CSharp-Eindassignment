d099a7e5779c175e56891f1215503b33228c7905
Simon <dutchsimon9@gmail.com>
2023-01-27 12:20:21 +0100

    Server Init

---
+++ ChatApplication/ChatServer/Udp/UdpListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ChatServer.Udp
+{
+    //Server
+    internal class UdpListener : UdpBase
+    {
+        private IPEndPoint _listenOn;
+
+        public UdpListener() : this(new IPEndPoint(IPAddress.Any, 32123))
+        {
+        }
+
+        public UdpListener(IPEndPoint endpoint)
+        {
+            _listenOn = endpoint;
+            Client = new UdpClient(_listenOn);
+        }
+
+        public void Reply(string message, IPEndPoint endpoint)
+        {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length, endpoint);
+        }
+    }
+}
