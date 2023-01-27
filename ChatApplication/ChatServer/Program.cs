5fe6ca153b4a073e70cb7fb79c6b3923f738086d
Moustapha Azaïmi <m.azaimi@student.avans.nl>
2023-01-27 12:11:42 +0100

    created temp server

--- ChatApplication/ChatServer/Program.cs
+++ ChatApplication/ChatServer/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
-Console.WriteLine("Hello, World!");
+namespace ChatServer
+{
+    class Program
+    {
+        static TcpListener _listener;
+
+        static void Main(string[] args)
+        {
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7899);
+            _listener.Start();
+            
+            var client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client connected!");
+        }
+    }
+}
