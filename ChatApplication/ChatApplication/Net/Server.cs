37ab179a3b0222d5d3ec3ccbcdd0254632d26d3b
Moustapha Azaïmi <m.azaimi@student.avans.nl>
2023-01-27 14:05:43 +0100

    created NET package

---
+++ ChatApplication/ChatApplication/Net/Server.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+
+namespace ChatApplication.Net;
+
+public class Server
+{
+    private TcpClient _client;
+    public Server()
+    {
+        _client = new TcpClient();
+    }
+
+    public void ConnectToServer(string username)
+    {
+        if (!_client.Connected)
+        {
+            _client.Connect("127.0.0.1", 7899);
+            var connectPacket = new PacketBuilder();
+            connectPacket.WriteOpCode(0);
+            connectPacket.WriteMessage(username);
+            _client.Client.Send(connectPacket.GetPacketBytes());
+        }
+    }
+}
